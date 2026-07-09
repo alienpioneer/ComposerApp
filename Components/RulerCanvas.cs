1b703010217dce369203369435d6b874e0366581
Alex <alienpioneer@gmail.com>
2026-07-09 23:46:26 +0200

    Consolidate rulers creation

--- Components/RulerCanvas.cs
+++ Components/RulerCanvas.cs
@@ -19,6 +19,12 @@ namespace AppComposer.Components
             typeof(RulerCanvas),
             new FrameworkPropertyMetadata(10.0, FrameworkPropertyMetadataOptions.AffectsRender));
 
+        public static readonly DependencyProperty RulerSizeProperty = DependencyProperty.Register(
+            nameof(RulerSize),
+            typeof(int),
+            typeof(RulerCanvas),
+            new FrameworkPropertyMetadata(20, FrameworkPropertyMetadataOptions.AffectsRender));
+
         public double Dpi
         {
             get => (double)GetValue(DpiProperty);
@@ -31,12 +37,30 @@ namespace AppComposer.Components
             set => SetValue(FontSizeProperty, value);
         }
 
+        public int RulerSize
+        {
+            get => (int)GetValue(RulerSizeProperty);
+            set => SetValue(RulerSizeProperty, value);
+        }
+
         public bool IsVertical { get; set; } = false;
 
         protected override void OnRender(DrawingContext drawContext)
         {
-            base.OnRender(drawContext); // Draws the existing Background brush
+            base.OnRender(drawContext);
 
+            Brush rulerBrush = CreateRulerTicks(IsVertical);
+
+            drawContext.DrawRectangle(
+                rulerBrush,
+                null,
+                new Rect(0, 0, ActualWidth, ActualHeight));
+
+            DrawLabels(drawContext);
+        }
+
+        private void DrawLabels(DrawingContext drawContext)
+        {
             double pixelsPerCm = Dpi / 2.54;
             double length = IsVertical ? ActualHeight : ActualWidth;
 
@@ -61,6 +85,66 @@ namespace AppComposer.Components
 
                 drawContext.DrawText(text, position);
             }
+        }
+
+        private Brush CreateRulerTicks(bool isVertical)
+        {
+            double pixelsPerCm = Dpi / 2.54;
+            double pixelsPerMm = pixelsPerCm / 10.0;
+
+            Rect backgroundFrame;
+
+            if (isVertical)
+            {
+                backgroundFrame = new Rect(0, 0, RulerSize, pixelsPerCm);
+            }
+            else
+            {
+                backgroundFrame = new Rect(0, 0, pixelsPerCm, RulerSize);
+            }
+
+            var background = new GeometryDrawing
+            {
+                Brush = Brushes.LightGray,
+                Geometry = new RectangleGeometry(backgroundFrame)
+            };
+
+            var geometryGroup = new GeometryGroup();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                double lineHeight = i == 0 ? RulerSize / 2 :
+                                    i == 5 ? RulerSize / 3 : RulerSize / 4;
+
+                if (isVertical)
+                {
+                    geometryGroup.Children.Add(new LineGeometry(new Point(RulerSize - lineHeight, i * pixelsPerMm),
+                                                                new Point(RulerSize, i * pixelsPerMm)));
+                }
+                else
+                {
+                    geometryGroup.Children.Add(new LineGeometry(new Point(i * pixelsPerMm, RulerSize),
+                                                                new Point(i * pixelsPerMm, RulerSize - lineHeight)));
+                }
+            }
+
+            var rulerLines = new GeometryDrawing
+            {
+                Pen = new Pen(Brushes.Black, 1),
+                Geometry = geometryGroup
+            };
+
+            var drawingGroup = new DrawingGroup();
+            drawingGroup.Children.Add(background);
+            drawingGroup.Children.Add(rulerLines);
+
+            return new DrawingBrush(drawingGroup)
+            {
+                TileMode = TileMode.Tile,
+                Viewport = backgroundFrame,
+                ViewportUnits = BrushMappingMode.Absolute,
+                Stretch = Stretch.None
+            };
         }
     }
 }
--- Models/CanvasSettings.cs
+++ Models/CanvasSettings.cs
@@ -21,7 +21,7 @@ namespace AppComposer.Models
 
         public int GridSize { get; set; } = 10;
 
-        public int RulerSize { get; set; } = 20;
+        public int RulerBandSize { get; set; } = 20;
         public int RulerFontSize { get; set; } = 10;
         public int Dpi { get; set; } = 96;
 
@@ -66,22 +66,6 @@ namespace AppComposer.Models
             }
         }
 
-        public Brush HorizontalRulerBrush
-        {
-            get
-            {
-                return CreateRulerBrush(Dpi, false);
-            }
-        }
-
-        public Brush VerticalRulerBrush
-        {
-            get
-            {
-                return CreateRulerBrush(Dpi, true);
-            }
-        }
-
         private Brush CreateGridBrush(int gridSize)
         {
             var geometry = new GeometryGroup();
@@ -109,66 +93,6 @@ namespace AppComposer.Models
                 TileMode = TileMode.Tile,
                 Viewport = new Rect(0, 0, gridSize, gridSize),
                 ViewportUnits = BrushMappingMode.Absolute
-            };
-        }
-
-        private Brush CreateRulerBrush(double dpi, bool isVertical)
-        {
-            double pixelsPerCm = dpi / 2.54;
-            double pixelsPerMm = pixelsPerCm / 10.0;
-
-            Rect backgroundFrame;
-
-            if (isVertical)
-            {
-                backgroundFrame = new Rect(0, 0, RulerSize, pixelsPerCm);
-            }
-            else 
-            {
-                backgroundFrame = new Rect(0, 0, pixelsPerCm, RulerSize);
-            }
-
-            var background = new GeometryDrawing
-            {
-                Brush = Brushes.LightGray,
-                Geometry = new RectangleGeometry(backgroundFrame)
-            };
-
-            var geometryGroup = new GeometryGroup();
-
-            for (int i=0; i<=10; i++)
-            {
-                double lineHeight = i==0 ? RulerSize/2 :
-                                    i==5 ? RulerSize/3 : RulerSize/4;
-
-                if (isVertical)
-                {
-                    geometryGroup.Children.Add(new LineGeometry(new Point(RulerSize - lineHeight, i * pixelsPerMm),
-                                                                new Point(RulerSize, i * pixelsPerMm)));
-                }
-                else
-                {
-                    geometryGroup.Children.Add(new LineGeometry(new Point(i*pixelsPerMm, RulerSize),
-                                                                new Point(i*pixelsPerMm, RulerSize-lineHeight)));
-                }
-            }
-
-            var rulerLines = new GeometryDrawing
-            {
-                Pen = new Pen(Brushes.Black, 1),
-                Geometry = geometryGroup
-            };
-
-            var drawingGroup = new DrawingGroup();
-            drawingGroup.Children.Add(background);
-            drawingGroup.Children.Add(rulerLines);
-
-            return new DrawingBrush(drawingGroup)
-            {
-                TileMode = TileMode.Tile,
-                Viewport = backgroundFrame,
-                ViewportUnits = BrushMappingMode.Absolute,
-                Stretch = Stretch.None
             };
         }
     }
